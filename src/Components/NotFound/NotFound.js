cc88f8cae0ab7c0df687296ffbaac1a473d054fd
v0idman7 <sergeyvoitkevich@mail.ru>
2021-07-23 23:02:56 +0300

    Add not found

---
+++ src/Components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import './NotFound.scss';
+
+function NotFound() {
+  return ( 
+    <div>
+      <span class="error">Page Not Found</span>
+      <span class="error__num">404</span>
+    </div>
+  );
+}
+
+export default NotFound;
