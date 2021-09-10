c1171ad043ca6445979ffd9846c99d4972c9deb0
v0idman7 <sergeyvoitkevich@mail.ru>
2021-09-10 12:58:25 +0300

    temporary change

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.scss';
-import App from './components/App';
+import App from './components/App.js';
 import store from './store'
 
 ReactDOM.render(
